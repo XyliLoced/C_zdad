384ed8ec0e34089a36889ea155f3b5d478fb4bbc
XyliLoced <143069617+XyliLoced@users.noreply.github.com>
2023-09-07 23:47:28 +0300

    Add files via upload

---
+++ 3.cs
@@ -0,0 +1,11 @@
+﻿internal class Programmm
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+            if (i % 2 == 0)
+                Console.WriteLine(i + "" , "");
+    }
+}
