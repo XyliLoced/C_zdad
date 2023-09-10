6c8ca27772d73033a40f8c4714e049f0c92524dc
XyliLoced <143069617+XyliLoced@users.noreply.github.com>
2023-09-11 00:03:59 +0300

    Add files via upload

---
+++ Program 11_09.cs
@@ -0,0 +1,28 @@
+﻿int Prompt(string msg)
+{
+
+System.Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+
+int fnumber(int number)
+{
+while (number > 999)
+{
+number /= 10;
+}
+return number % 10;
+}
+
+bool check(int number)
+{
+if (number < 100)
+return false;
+else return true;
+}
+
+if (check(number) != true)
+System.Console.WriteLine("Третьей цифры нет, число < 100");
+else
+System.Console.WriteLine($"Третьей цифрой числа {number}, является {fnumber(number)}");
